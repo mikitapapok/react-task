303971ad56cb512b771dd1d1092dca4614dbdfd2
Mikita Papok <73660686+mikitapapok@users.noreply.github.com>
2021-12-20 14:39:02 +0300

    init react-task-4

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './style/normalize.scss';
 import App from './App.jsx';
 
 ReactDOM.render(
-    <HashRouter >
-        <App />
-    </HashRouter>,
 
+        <HashRouter>
+            <App />
+        </HashRouter>
+,
     document.getElementById('root')
 );
